035aa47c0581b60aca8b286a7e962af46cb864d6
shargen <sharansh.sinha@gmail.com>
2024-06-11 10:22:01 +0530

    Revert "web analysys"
    This reverts commit 10362c914010979fdbcda5545a867228b489189c.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import Sidebar from "@/components/sidebar/sidebar";
 import Providers from "./providers";
 import ThemeSwitch from "@/components/panel/ThemeSwitch";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,7 +29,6 @@ export default function RootLayout({
           <Sidebar />
           {children}
           <SpeedInsights />
-          <Analytics />
         </Providers>
       </body>
     </html>
